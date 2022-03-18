9f5823b861e0cf9e8c21f4d31be5ef3b49bfbed1
nald-dev <rynvva@gmail.com>
2022-03-18 10:28:33 +0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# learn-express-postgresql-graphql
+
+```npm start``` to start the server then open http://localhost:4000/graphql to test the queries (Make sure your local database running and in the right configuration as the config in server.js)
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: "envision",
+  host: "localhost",
+  database: "postgres",
+  password: "",
+  port: 5432,
+  url: ""
+})
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  # A book has a title and an author
+  type Article {
+    title: String
+    content: String
+    account: Account
+  }
+
+  type Account {
+    username: String
+    articles: [Article]
+  }
+
+  type Query {
+    accounts: [Account]
+    articles: [Article]
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  articles: async() => {
+    let { rows: articlesSource } = await pool.query(`SELECT * FROM articles`)
+
+    const accounts = await root.accounts()
+
+    const articles = articlesSource.map(article => ({
+      ...article,
+      account: accounts.find(account => account.id === article.account_id)
+    }))
+
+    return articles
+  },
+  accounts: async() => {
+    let { rows: accountsSource } = (await pool.query(`SELECT id, username FROM accounts`))
+
+    const accounts = accountsSource.map(account => ({
+      ...account,
+      articles: async() => {
+        const articles = await root.articles()
+
+        return articles.filter(article => article.account_id === account.id)
+      }
+    }))
+
+    return accounts
+  }
+}
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}))
+
+app.listen(4000)
+
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
